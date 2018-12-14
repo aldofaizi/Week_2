e7c1ba7f9c42f0880a8f0d84caed4447cb56356c
aldofaizi <aldofaizi@gmail.com>
2018-12-14 11:13:29 +0700

    Adding exercise 4 switch case calendar

---
+++ W2-E4.js
@@ -0,0 +1,21 @@
+var tanggal = 7;
+var bulan = 1;
+var tahun = 1997;
+var mo
+
+
+switch(bulan){
+    case 1: { mo = 'Januari'; break;}
+    case 2: { mo = "Februari"; break;}
+    case 3: { mo = "Maret"; break;}
+    case 4: { mo = "April"; break;}
+    case 5: { mo = "Mei"; break;}
+    case 6: { mo = "Juni"; break;}
+    case 7: { mo = "Juli"; break;}
+    case 8: { mo = "Agustus"; break;}
+    case 9: { mo = "September"; break;}
+    case 10: { mo = "Oktober"; break;}
+    case 11: { mo = "November"; break;}
+    case 12: { mo = "Desember"; break;}
+}
+console.log(tanggal, mo, tahun);
