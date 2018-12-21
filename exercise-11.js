bc4bdd3dea4b20a15ad491f462abd1e5d1274e7d
aldofaizi <aldofaizi@gmail.com>
2018-12-21 20:08:49 +0700

    update multiple output

--- exercise-11.js
+++ exercise-11.js
@@ -1,6 +1,5 @@
-var terbalik = "";
-
 function balikKata(kata) {
+    var terbalik = ''
     for (var i = kata.length -1; i >= 0; i--) {
        terbalik += kata[i]
     }
@@ -8,8 +7,8 @@ function balikKata(kata) {
 }
   
 // TEST CASES
-//console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
 console.log(balikKata('John Doe')); // eoD nhoJ
-//console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
-//console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
-//console.log(balikKata('Super')); // repuS
+console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(balikKata('Super')); // repuS
